7de5e586aa07566ce270dacd0f30664e003ff5f6
sherbinsr <sherbinsyles31@gmail.com>
2023-05-01 14:46:22 +0530

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cse-department';
+
+  about()
+  {
+    scrollTo(0,100);
+  }
+
+  events()
+  {
+    scrollTo(0,1800);
+  }
+  contact()
+  {
+    scrollTo(0,2300);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
